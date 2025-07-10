94d616db00eff2ce643710d72876dda691d8d9de
Nathan <n49874687@gmail.com>
2025-07-10 15:27:11 +0200

    test: modification arguments fonction `get_moves`

--- tests/test_game.py
+++ tests/test_game.py
@@ -8,9 +8,9 @@ def test_chessboard_creation():
 def test_king_moves():
     b = blank_board()
     b[3][3] = King(WHITE)
-    moves = b[3][3].get_moves(3,3, b)
+    moves = b[3][3].get_moves(Position(3,3), b)
     assert len(moves) == 8
 
     board = ChessBoard()
-    moves = board.board[0][4].get_moves(4,0, board.board)
+    moves = board.board[0][4].get_moves(Position(4,0), board.board)
     assert len(moves) == 0
